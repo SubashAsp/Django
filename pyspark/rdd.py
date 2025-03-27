387e04fffd01707380818151858ad8c0758723d2
SubashAsp <subasht0731@gmail.com>
2025-03-27 18:17:09 +0530

    map function

--- pyspark/rdd.py
+++ pyspark/rdd.py
@@ -1,8 +1,14 @@
 from pyspark.sql import SparkSession
 
 spark=SparkSession.builder .appName('PySpark_Intro') .getOrCreate()
-print (spark)
+# print (spark)
 
+# numbers=[1, 2, 3, 4, 5, 6]
+# rdd=spark.sparkContext.parallelize(numbers)
+# print (rdd.collect())
+
+sc=spark.sparkContext
 numbers=[1, 2, 3, 4, 5, 6]
-rdd=spark.sparkContext.parallelize(numbers)
-print (rdd.collect())
+rdd=sc.parallelize(numbers)
+rdd1=rdd.map(lambda x:x*x)
+print (rdd1.collect())
